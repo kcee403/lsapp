92c3e5d7dfbe2c4513c0c4d3eca54cd47364a177
kcee403 <kcee403@gmail.com>
2018-01-28 05:07:59 -0500

    bootstrap/LeaguseFlysystemAS3.php & index.php with adapter

---
+++ bootstrap/LeagueFlysystemAwS3.php
@@ -0,0 +1,18 @@
+<?php
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+include __DIR__ . '/vendor/autoload.php';
+
+$client = new S3Client([
+    'credentials' => [
+        'key'    => 'AKIAILEH2PC56JEWGWQ',
+        'secret' => 'NDC0S1SRCAG4CwDQPQUOHkfAhQK1TauuqONumL5C'
+    ],
+    'region' => 'sts.us-east-1.amazonaws.com',
+    'version' => '5.5',
+]);
+
+$adapter = new AwsS3Adapter($client, 'your-bucket-name');
+$filesystem = new Filesystem($adapter);
