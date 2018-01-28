e18c2888dbc3f9bd3ec21776d4ec578d4f37b601
kcee403 <kcee403@gmail.com>
2018-01-28 08:32:09 -0500

    Edited S3ImageController.php, = Storage::disk()

--- bootstrap/LeagueFlysystemAwS3.php
+++ bootstrap/LeagueFlysystemAwS3.php
@@ -9,10 +9,11 @@ $client = new S3Client([
     'credentials' => [
         'key'    => 'AKIAILEH2PC56JEWGWQ',
         'secret' => 'NDC0S1SRCAG4CwDQPQUOHkfAhQK1TauuqONumL5C'
-        'bucket' => 'lara-file'
+
     ],
     'region' => 'us-east-1',
     'version' => '5.5',
+    'bucket' => 'lara-file'
 ]);
 
 $adapter = new AwsS3Adapter($client, 'lara-file');
